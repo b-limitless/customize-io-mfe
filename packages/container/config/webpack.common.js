17581885fcd4a9d66d0dde28f1dfe2428a301d97
Bharat Shah <akashche72@gmail.com>
2023-03-26 15:41:01 +0545

    starting container application for microfrontend

---
+++ packages/container/config/webpack.common.js
@@ -0,0 +1,66 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { merge } = require("webpack-merge");
+
+const commonConfig = {
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      }, 
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react", "@babel/preset-env"],
+            plugins: ["@babel/plugin-transform-runtime"],
+          },
+        },
+      },
+      
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".tsx", ".ts"]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ]
+};
+
+
+const sassConfig = {
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+};
+
+const cssConfig =  {
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
+
+module.exports = merge(commonConfig, sassConfig, cssConfig);
