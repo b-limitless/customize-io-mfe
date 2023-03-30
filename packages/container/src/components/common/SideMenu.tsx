ce320527aed74bc0e29f4dd3c5cfd4406b39a05a
Bharat Shah <akashche72@gmail.com>
2023-03-30 16:54:56 +0545

    worked on menu for the small screen so that I can hide all title as it shows in large and small screen, only icon and when the hover they clicked then and then only show the child menu or its sub menu

--- packages/container/src/components/common/SideMenu.tsx
+++ packages/container/src/components/common/SideMenu.tsx
@@ -9,7 +9,7 @@ import avatar from "../../assets/img/avatar.png";
 import arrowDown from "../../assets/svg/arrow-down.svg";
 import logout from "../../assets/svg/logout.svg";
 import logoText from "../../assets/svg/logo-text.svg";
-import logoIcon from "../../assets/svg/logo-icon.svg"; 
+import logoIcon from "../../assets/svg/logo-icon.svg";
 // logo-text
 // logo-icon
 
@@ -23,7 +23,7 @@ export default function SideMenu({ }: Props) {
           <div className="row logo--arrow">
             <div className="item logo">
               <img src={logoIcon} alt="" />
-              <img src={logoText} alt="" />
+              <img src={logoText} alt="" className="logo-text" />
               {/* <img src={logoPrimary} alt="Customize io" /> */}
             </div>
             <div className="item arrow">
@@ -31,41 +31,42 @@ export default function SideMenu({ }: Props) {
             </div>
           </div>
 
+
           <div className="row navigation">
             <ul className="navigation--ul">
               {/* Will create data structure to handle the menu in dynamic way */}
               {/* Where we can assign root menu and its child menu */}
               {/* We should be able to load the component as well */}
               <li className="item base-list">
-              <input type="radio" name="ci-root-menu" id="cio-product-li-0" className="major-list" />
+                <input type="radio" name="ci-root-menu" id="cio-product-li-0" className="major-list" />
                 <label className="title-icon" htmlFor="cio-product-li-0">
                   <span className="icon"><img src={productIcon} /></span>
                   <span className="title">Dashboard</span>
-          
+
                 </label>
               </li>
               <li className="item base-list">
                 <input type="radio" name="ci-root-menu" id="cio-product-li" className="major-list" />
                 <label className="title-icon" htmlFor="cio-product-li">
-                  <span className="icon"><img src={productIcon} /></span>
+                  <span className="icon"><img src={productIcon} className="root-menu-icon" /></span>
                   <span className="title">Products</span>
                   <span className="arrow"><img src={arrowDown} /></span>
                 </label>
 
 
                 <ul className="sub--ul">
-                  <input type="radio" name="sub-menu-checkbox" id="ci-title-one" className="sub-menu-checkbox"/>
-                  <label htmlFor="ci-title-one"> 
+                  <input type="radio" name="sub-menu-checkbox" id="ci-title-one" className="sub-menu-checkbox" />
+                  <label htmlFor="ci-title-one">
                     <li className="sub--li">Title One</li>
                   </label>
 
-                  <input type="radio" name="sub-menu-checkbox" id="ci-title-two" className="sub-menu-checkbox"/>
-                  <label htmlFor="ci-title-two"> 
+                  <input type="radio" name="sub-menu-checkbox" id="ci-title-two" className="sub-menu-checkbox" />
+                  <label htmlFor="ci-title-two">
                     <li className="sub--li">Title Two</li>
                   </label>
 
-                  <input type="radio" name="sub-menu-checkbox" id="ci-title-three" className="sub-menu-checkbox"/>
-                  <label htmlFor="ci-title-three"> 
+                  <input type="radio" name="sub-menu-checkbox" id="ci-title-three" className="sub-menu-checkbox" />
+                  <label htmlFor="ci-title-three">
                     <li className="sub--li">Title Three</li>
                   </label>
 
@@ -76,22 +77,22 @@ export default function SideMenu({ }: Props) {
                 <input type="radio" name="ci-root-menu" id="cio-product-li-1" className="major-list" />
                 <label className="title-icon" htmlFor="cio-product-li-1">
                   <span className="icon"><img src={productIcon} /></span>
-                  <span className="title">Products</span>
+                  <span className="title">Pricing</span>
                   <span className="arrow"><img src={arrowDown} /></span>
                 </label>
                 <ul className="sub--ul">
-                  <input type="radio" name="sub-menu-checkbox" id="ci-title-4" className="sub-menu-checkbox"/>
-                  <label htmlFor="ci-title-4"> 
+                  <input type="radio" name="sub-menu-checkbox" id="ci-title-4" className="sub-menu-checkbox" />
+                  <label htmlFor="ci-title-4">
                     <li className="sub--li">Title One</li>
                   </label>
 
-                  <input type="radio" name="sub-menu-checkbox" id="ci-title-5" className="sub-menu-checkbox"/>
-                  <label htmlFor="ci-title-5"> 
+                  <input type="radio" name="sub-menu-checkbox" id="ci-title-5" className="sub-menu-checkbox" />
+                  <label htmlFor="ci-title-5">
                     <li className="sub--li">Title Two</li>
                   </label>
 
-                  <input type="radio" name="sub-menu-checkbox" id="ci-title-6" className="sub-menu-checkbox"/>
-                  <label htmlFor="ci-title-6"> 
+                  <input type="radio" name="sub-menu-checkbox" id="ci-title-6" className="sub-menu-checkbox" />
+                  <label htmlFor="ci-title-6">
                     <li className="sub--li">Title Three</li>
                   </label>
 
@@ -110,8 +111,8 @@ export default function SideMenu({ }: Props) {
               </div>
               <div className="col details">
                 <div className="details--item switch">Switch account
-                  <br/>to <span>Pro</span></div>
-                <div className="details--item unlinited">Unlimited access to <br/>biggest 3d models service <br/>with highest quality</div>
+                  <br />to <span>Pro</span></div>
+                <div className="details--item unlinited">Unlimited access to <br />biggest 3d models service <br />with highest quality</div>
                 <div className="details--item ico-getpro">
                   <div className="get-pro">Get your Pro</div>
                   <div className="icon-right">
@@ -121,15 +122,32 @@ export default function SideMenu({ }: Props) {
               </div>
             </div>
             <div className="row item">
-              <div className="icon">
-                <img src={setting} />
-              </div>
-              <div className="text settings">Settings</div>
+              <input type="radio"
+                name="bottom-checkbox"
+                id="settings"
+                className="bottom-checkbox" />
+
+              <label htmlFor="settings">
+                <div className="icon">
+                  <img src={setting} />
+                </div>
+              </label>
+
+
+
+              <div className="text settings title">Settings</div>
             </div>
             <div className="row item">
-              <div className="icon">
-                <img src={setting} />
-              </div>
+              <input type="radio"
+                name="bottom-checkbox"
+                id="faq-checkbox"
+                className="bottom-checkbox" />
+              <label htmlFor="faq-checkbox">
+                <div className="icon">
+                  <img src={setting} />
+                </div>
+              </label>
+
               <div className="text settings">FAQ</div>
             </div>
           </div>
@@ -144,7 +162,7 @@ export default function SideMenu({ }: Props) {
             <div className="col logout">
               <img src={logout} alt="" />
             </div>
-            
+
           </div>
 
         </div>
