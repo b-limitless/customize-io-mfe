5238bb33f42cedf53021a1bded715832c26b066e
Bharat Shah <akashche72@gmail.com>
2023-06-11 19:28:02 +0545

    Updates

---
+++ packages/common-functions/functions/firstLetterUpperCase.ts
@@ -0,0 +1,5 @@
+const firstLetterUpperCase = (str:string) => {
+    return str[0].toUpperCase() + str.slice(1);
+}
+
+export default firstLetterUpperCase
---
+++ packages/common-functions/functions/index.ts
@@ -0,0 +1,1 @@
+export  {default as  firstLetterUpperCase} from "./firstLetterUpperCase";
