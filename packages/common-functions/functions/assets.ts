41668924511342a1a681cdc7594a7e89b9bd55e8
Bharat Shah <akashche72@gmail.com>
2023-06-21 18:54:06 +0545

    cdn link is added

---
+++ packages/common-functions/functions/assets.ts
@@ -0,0 +1,18 @@
+export const pngCDNAssetsURIs = {
+  febric1:
+    "http://res.cloudinary.com/dun5p8e5d/image/upload/v1685971320/lgzm022qmy39xq1xaidx.png",
+};
+
+export const svgCDNAssets = {
+  crossIcon:
+    "http://res.cloudinary.com/dun5p8e5d/image/upload/v1685971300/fybtpbqt0cnalvwddcul.svg",
+  arrowUpIcon:
+    "http://res.cloudinary.com/dun5p8e5d/image/upload/v1686037397/ex0zcv1melpoth1tn4xk.svg",
+  primiumIcon:
+    "https://res.cloudinary.com/dun5p8e5d/image/upload/v1686037393/mpeshbmwy0lskv1argk5.svg",
+  plusDark:
+    "http://res.cloudinary.com/dun5p8e5d/image/upload/v1686134129/i2p3z5mkvqs2dbsbul40.svg",
+  delete:
+    "http://res.cloudinary.com/dun5p8e5d/image/upload/v1686142810/yowpwbrvm9dy6tx7r10p.svg",
+  successCheck: "http://res.cloudinary.com/dun5p8e5d/image/upload/v1686557298/fxrmxjkkd4eb5mqhchdw.svg"
+};
