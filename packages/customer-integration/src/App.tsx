7df48cef5618f68418a4d9181ddc0061fd235ab2
Bharat Shah <akashche72@gmail.com>
2023-07-20 16:34:21 +0545

    updating app js with only component welcome based on condition

--- packages/customer-integration/src/App.tsx
+++ packages/customer-integration/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '@styles/index.scss';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Welcome from '@components/Welcome';
@@ -8,15 +8,32 @@ import { Link } from 'react-router-dom';
 
 type Props = {}
 
+enum componentEnum  {
+  welcome='welcome',
+  agents='agents', 
+  bookAnAppointment='bookAnAppointment'
+}
+
+type componentType = `${componentEnum}`; 
+
 export default function App({ }: Props) {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Welcome />}></Route>
-        <Route path="/welcome" element={<Welcome />}></Route>
-        <Route path="/agents" element={<ListAgents />}></Route>
-        <Route path="/book-an-appointment" element={<BookAnAppointment />}></Route>
-      </Routes>
-    </BrowserRouter>
-  )
+  const [selectedComponent, setSeletedComponent] = useState<componentType>('welcome');
+
+  if(selectedComponent === 'welcome') {
+    return <Welcome/>
+  }
+
+  return null;
+
+  // return (
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/" element={<Welcome />}></Route>
+    //     <Route path="/agents" element={<ListAgents />}></Route>
+    //     <Route path="/book-an-appointment" element={<BookAnAppointment />}></Route>
+    //   </Routes>
+    // </BrowserRouter>
+
+
+  // )
 }
