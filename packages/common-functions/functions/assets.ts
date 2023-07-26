2d4e619d1a533db1206b711bb1ea3ed47e14b2b1
Bharat Shah <akashche72@gmail.com>
2023-07-26 13:38:11 +0545

    adding logos links

--- packages/common-functions/functions/assets.ts
+++ packages/common-functions/functions/assets.ts
@@ -13,5 +13,12 @@ export const svgCDNAssets = {
     "http://res.cloudinary.com/dun5p8e5d/image/upload/v1686134129/i2p3z5mkvqs2dbsbul40.svg",
   delete:
     "http://res.cloudinary.com/dun5p8e5d/image/upload/v1686142810/yowpwbrvm9dy6tx7r10p.svg",
-  successCheck: "http://res.cloudinary.com/dun5p8e5d/image/upload/v1686557298/fxrmxjkkd4eb5mqhchdw.svg"
+  successCheck:
+    "http://res.cloudinary.com/dun5p8e5d/image/upload/v1686557298/fxrmxjkkd4eb5mqhchdw.svg",
+  logoText:
+    "http://res.cloudinary.com/dun5p8e5d/image/upload/v1690289934/eusitlt2a0lg8xstxl3z.svg",
+  logoPrimary:
+    "http://res.cloudinary.com/dun5p8e5d/image/upload/v1690289931/o1dmiexebk6ezojcy57t.svg",
+  logoIcon:
+    "http://res.cloudinary.com/dun5p8e5d/image/upload/v1690289927/jjszlaw9jpvccr5rzgv9.svg",
 };
